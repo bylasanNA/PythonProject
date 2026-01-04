68edee8b5e5ff882d332b71acc1c59839429f503
bna <bylasanna916@gmail.com>
2026-01-04 08:28:20 +0200

    Final project

--- qa_python/apple_final_project/tests/all_tests.py
+++ qa_python/apple_final_project/tests/all_tests.py
@@ -27,40 +27,50 @@ class AllTests(unittest.TestCase):
     def test_search_for_a_product(self):
         self.main_page.search_for_product(PRODUCT)
         url = self.driver.current_url
-        assert "https://www.apple.com/il/search/" in url, "URL is not as expected."
+        assert "https://www.apple.com/il/search/" in url, "Search page URL is not as expected."
 
         self.search_page.check_results_found_number_plus_assertion()
 
-    def test_main_buttons_exist(self):
+        print("Search for a product test was successful." )
+
+    def test_count_navigation_buttons(self):
         buttons_count = self.main_page.check_navigation_buttons()
         assert buttons_count == 6, "Unexpected number of navigation buttons."
 
-    def test_apple_logo_works(self):
+        print("Count navigation buttons test was successful.")
+
+    def test_apple_logo_button_works(self):
         self.main_page.mac_button()
         url = self.driver.current_url
-        assert url == "https://www.apple.com/il/mac/", "URL is not as expected."
+        assert url == "https://www.apple.com/il/mac/", "Mac page URL is not as expected."
 
         self.mac_page.apple_logo()
         url = self.driver.current_url
-        assert url == "https://www.apple.com/il/", "URL is not as expected."
+        assert url == "https://www.apple.com/il/", "Main page URL is not as expected."
+
+        print("Apple logo button works test was successful.")
 
     def test_text_is_found_in_page(self):
         self.main_page.where_to_buy_button()
         url = self.driver.current_url
-        assert url == "https://www.apple.com/il/buy/", "URL is not as expected."
+        assert url == "https://www.apple.com/il/buy/", "Where to buy page URL is not as expected."
 
         self.where_to_buy_page.find_a_reseller_button()
         url = self.driver.current_url
-        assert url == "https://locate.apple.com/il/en/sales", "URL is not as expected."
+        assert url == "https://locate.apple.com/il/en/sales", "Find a reseller page URL is not as expected."
 
         results_as_text = self.find_a_reseller_page.search_location_and_product(LOCATION)
         assert "sales locations near" in results_as_text, "Unexpected text after searching for product."
 
+        print("Text found in page test was successful.")
+
     def test_find_a_store_correct_page(self):
         self.main_page.learn_more_button()
         url = self.driver.current_url
-        assert url == "https://www.apple.com/il/iphone-17-pro/", "URL is not as expected."
+        assert url == "https://www.apple.com/il/iphone-17-pro/", "Learn more page URL is not as expected."
 
         self.learn_more_page.find_a_store_button()
         url = self.driver.current_url
-        assert url == "https://www.apple.com/il/buy/?iphone", "URL is not as expected."
+        assert url == "https://www.apple.com/il/buy/?iphone", "Find a store page URL is not as expected."
+
+        print("Find a store correct page test was successful.")
